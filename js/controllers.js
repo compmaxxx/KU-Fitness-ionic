b0a63c7d419c3ec0e03643114512a59443b91438
compmaxxx <compmaxxx@gmail.com>
2015-03-21 18:26:45 +0700

    refactor service

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,10 @@
 angular.module('starter.controllers', [])
 
 .controller('ResultCtrl', function($scope, Results) {
-	$scope.results = Results.all();
-	$scope.remove = function(result) {
-		Results.remove(result);
-	}
+  $scope.results = Results.all();
+  $scope.remove = function(result) {
+    Results.remove(result);
+  }
 })
 
 .controller('TabsCtrl', function($scope) {
@@ -12,30 +12,30 @@ angular.module('starter.controllers', [])
 })
 
 .controller('CourseCtrl', function($scope, Courses) {
-	Courses.all().then(function(data) {
-		$scope.courses = data;
-	});
-	$scope.remove = function(chat) {
-		Courses.remove(chat.id);
-	}
+  Courses.all().then(function(data) {
+    $scope.courses = data;
+  });
+  $scope.remove = function(chat) {
+    Courses.remove(chat.id);
+  }
 })
 
-.controller('CourseDetailCtrl', function($scope, $stateParams, Courses) {
-	var course_id = $stateParams.course_id;
-	$scope.course = Courses.get(course_id);
-	Courses.get_tests(course_id).then(function(data) {
-		$scope.tests = data;
-	});
+.controller('CourseDetailCtrl', function($scope, $stateParams, Courses, Tests) {
+  var course_id = $stateParams.course_id;
+  $scope.course = Courses.get(course_id);
+  Tests.ByCourse(course_id).then(function(data) {
+    $scope.tests = data;
+  });
 })
 
-.controller('TestDetailCtrl', function($scope, $stateParams, Courses) {
-	var test_id = $stateParams.test_id;
-	$scope.test = Courses.get_test(test_id);
+.controller('TestDetailCtrl', function($scope, $stateParams, Tests) {
+  var test_id = $stateParams.test_id;
+  $scope.test = Tests.get(test_id);
 
 })
 
 .controller('SettingCtrl', function($scope) {
-	$scope.settings = {
-		enableFriends: true
-	};
+  $scope.settings = {
+    enableFriends: true
+  };
 });
--- js/services.js
+++ js/services.js
@@ -1,119 +1,129 @@
 angular.module('starter.services', [])
 
 .factory('Courses', function($http, $q) {
-	// Might use a resource here that returns a JSON array
+  // Might use a resource here that returns a JSON array
 
-	// Some fake testing data
+  // Some fake testing data
 
-	var url = 'http://158.108.34.49:8008/course-rest/';
-	var course_all = 'view-all';
-	var course_detail = 'view';
-	var course_test = 'view-tests';
+  var url = 'http://' + Config.HOST + ':8008/course-rest';
 
-	return {
-		all: function() {
-			var deferred = $q.defer()
-			$http.get(url + course_all)
-				.success(function(data, status, headers, config) {
-					courses = data;
-					deferred.resolve(data)
-				})
-				.error(function(data, status, headers, config) {
-					//handle error
-					alert('Cant\'t connnect to server');
-				});
-			return deferred.promise
-		},
-		get_tests: function(course_id) {
-			var deferred = $q.defer()
-			$http.get(url + course_test, {
-					params: {
-						id: course_id
-					}
-				})
-				.success(function(data, status, headers, config) {
-					tests = data;
-					deferred.resolve(data)
-				})
-				.error(function(data, status, headers, config) {
-					//handle error
-					alert('Cant\'t connnect to server');
-				});
-			return deferred.promise
-		},
-		get_test: function(test_id) {
-			for (var i = 0; i < tests.length; i++) {
-				if (tests[i].id === parseInt(test_id)) {
-					return tests[i];
-				}
-			}
-			return null;
-		},
-		remove: function(course) {
-			courses.splice(courses.indexOf(course), 1);
-		},
-		get: function(course_id) {
-			for (var i = 0; i < courses.length; i++) {
-				if (courses[i].id === parseInt(course_id)) {
-					return courses[i];
-				}
-			}
-			return null;
-		}
-	};
+  return {
+    all: function() {
+      var deferred = $q.defer()
+      $http({
+          url: url,
+          method: 'GET',
+        })
+        .success(function(data, status, headers, config) {
+          courses = data;
+          deferred.resolve(data)
+        })
+        .error(function(data, status, headers, config) {
+          //handle error
+          alert('Cant\'t connnect to server');
+        });
+      return deferred.promise
+    },
+
+    remove: function(course) {
+      courses.splice(courses.indexOf(course), 1);
+    },
+    get: function(course_id) {
+      for (var i = 0; i < courses.length; i++) {
+        if (courses[i].id === parseInt(course_id)) {
+          return courses[i];
+        }
+      }
+      return null;
+    }
+  };
+})
+
+.factory('Tests', function($http, $q) {
+  var url = 'http://' + Config.HOST + ':8008/test-rest';
+  return {
+    ByCourse: function(course_id) {
+      var deferred = $q.defer()
+      $http({
+          url: url,
+          method: 'GET',
+          data: {
+            'id': course_id
+          }
+        })
+        .success(function(data, status, headers, config) {
+          tests = data;
+          deferred.resolve(data)
+        })
+        .error(function(data, status, headers, config) {
+          //handle error
+          alert('Cant\'t connnect to server');
+        });
+      return deferred.promise
+    },
+    get: function(test_id) {
+      for (var i = 0; i < tests.length; i++) {
+        if (tests[i].id === parseInt(test_id)) {
+          return tests[i];
+        }
+      }
+      return null;
+    },
+  }
+
 })
 
 .factory('Results', function() {
-	// Might use a resource here that returns a JSON array
+  // Might use a resource here that returns a JSON array
 
-	// Some fake testing data
-	var results = [{
-		tester_tag: 0,
-		course_name: 'IWING Course',
-		test_name: 'ชั่งน้ำหนัก-BMI',
-		value: '67.5',
-		unit: 'กิโลกรัม'
-	}, {
-		tester_tag: 1,
-		course_name: 'IWING Course',
-		test_name: 'ชั่งน้ำหนัก-BMI',
-		value: '46.95',
-		unit: 'กิโลกรัม'
-	}, {
-		tester_tag: 2,
-		course_name: 'IWING Course',
-		test_name: 'วัดส่วนสูง-BMI',
-		value: '159',
-		unit: 'เซนติเมตร'
-	}, {
-		tester_tag: 3,
-		course_name: 'IWING Course',
-		test_name: 'จำนวนเที่ยว-Pacer',
-		value: '44',
-		unit: 'เที่ยว'
-	}, {
-		id: 4,
-		tester_tag: 4,
-		course_name: 'IWING Course',
-		test_name: 'Curl Up-Curl Up',
-		value: '50',
-		unit: 'รอบ'
-	}];
+  // Some fake testing data
+  var results = [{
+    tester_tag: 0,
+    course_name: 'IWING Course',
+    test_name: 'ชั่งน้ำหนัก-BMI',
+    value: '67.5',
+    unit: 'กิโลกรัม'
+  }, {
+    tester_tag: 1,
+    course_name: 'IWING Course',
+    test_name: 'ชั่งน้ำหนัก-BMI',
+    value: '46.95',
+    unit: 'กิโลกรัม'
+  }, {
+    tester_tag: 2,
+    course_name: 'IWING Course',
+    test_name: 'วัดส่วนสูง-BMI',
+    value: '159',
+    unit: 'เซนติเมตร'
+  }, {
+    tester_tag: 3,
+    course_name: 'IWING Course',
+    test_name: 'จำนวนเที่ยว-Pacer',
+    value: '44',
+    unit: 'เที่ยว'
+  }, {
+    id: 4,
+    tester_tag: 4,
+    course_name: 'IWING Course',
+    test_name: 'Curl Up-Curl Up',
+    value: '50',
+    unit: 'รอบ'
+  }];
 
-	return {
-		all: function() {
-			return results;
-		},
-		remove: function(chat) {
-			results.splice(results.indexOf(chat), 1);
-		},
-		get: function(result_id) {
-			for (var i = 0; i < results.length; i++) {
-				if (results[i].id === parseInt(result_id)) {
-					return results[i];
-				}
-			}
-			return null;
-		}
-	};
+  return {
+    all: function() {
+      return results;
+    },
+    remove: function(chat) {
+      results.splice(results.indexOf(chat), 1);
+    },
+    get: function(result_id) {
+      for (var i = 0; i < results.length; i++) {
+        if (results[i].id === parseInt(result_id)) {
+          return results[i];
+        }
+      }
+      return null;
+    }
+  };
 })
