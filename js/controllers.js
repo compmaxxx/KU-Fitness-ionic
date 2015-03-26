e220f330eb19d0ea9ad148ad8cc68f76bf8c3680
compmaxxx <compmaxxx@gmail.com>
2015-03-26 12:42:57 +0700

    handle not regis tester

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,10 @@
 angular.module('starter.controllers', [])
 
 .controller('ResultCtrl', function($scope, Results) {
-  $scope.results = Results.all();
-  $scope.remove = function(result) {
-    Results.remove(result);
-  }
+	$scope.results = Results.all();
+	$scope.remove = function(result) {
+		Results.remove(result);
+	}
 })
 
 .controller('TabsCtrl', function($scope) {
@@ -12,107 +12,123 @@ angular.module('starter.controllers', [])
 })
 
 .controller('CourseCtrl', function($scope, Courses) {
-  Courses.all().then(function(data) {
-    $scope.courses = data;
-  });
+	Courses.all().then(function(data) {
+		$scope.courses = data;
+	});
 })
 
 .controller('CourseDetailCtrl', function($scope, $stateParams, Courses, Tests) {
-  var course_id = $stateParams.course_id;
-  $scope.course = Courses.get(course_id);
-  Tests.ByCourse(course_id).then(function(data) {
-    $scope.tests = data;
-  });
+	var course_id = $stateParams.course_id;
+	$scope.course = Courses.get(course_id);
+	Tests.ByCourse(course_id).then(function(data) {
+		$scope.tests = data;
+	});
 })
 
 .controller('TestDetailCtrl', function($scope, $stateParams, Tests, Results,
-  $ionicPopup, $q) {
-  var test_id = $stateParams.test_id;
-  var course_id = $stateParams.course_id;
-  $scope.test = Tests.get(test_id);
-  $scope.result = {};
+	$ionicPopup, $q) {
+	var test_id = $stateParams.test_id;
+	var course_id = $stateParams.course_id;
+	$scope.test = Tests.get(test_id);
+	$scope.result = {};
 
-  var showAlert = function(title, message) {
-    var deferred = $q.defer();
-    var alertPopup = $ionicPopup.alert({
-      title: title,
-      template: message
-    });
-    alertPopup.then(function(res) {;
-      deferred.resolve(res);
-    });
+	var showAlert = function(title, message) {
+		var deferred = $q.defer();
+		var alertPopup = $ionicPopup.alert({
+			title: title,
+			template: message
+		});
+		alertPopup.then(function(res) {;
+			deferred.resolve(res);
+		});
 
-    return deferred.promise;
-  };
+		return deferred.promise;
+	};
 
-  var showConfirm = function(title, message) {
-    var deferred = $q.defer();
-    var confirmPopup = $ionicPopup.confirm({
-      title: title,
-      template: message
-    });
-    confirmPopup.then(function(res) {
-      if (res) {
-        deferred.resolve(res);
-      } else {
-        deferred.reject(res);
-      }
-    });
+	var showConfirm = function(title, message) {
+		var deferred = $q.defer();
+		var confirmPopup = $ionicPopup.confirm({
+			title: title,
+			template: message
+		});
+		confirmPopup.then(function(res) {
+			if (res) {
+				deferred.resolve(res);
+			} else {
+				deferred.reject(res);
+			}
+		});
 
-    return deferred.promise;
-  };
+		return deferred.promise;
+	};
 
-  $scope.AlertTestInput = function(data) {
-    showAlert('สำเร็จ', 'บันทึกข้อมูลเรียบร้อย')
-      .then(function(data) {
-        $scope.clear();
-      });
-  }
+	$scope.AlertTestInput = function(data) {
+		showAlert('สำเร็จ', 'บันทึกข้อมูลของ ' + $scope.result.tester_tag +
+				' เรียบร้อย')
+			.then(function(data) {
+				$scope.clear();
+			});
+	}
 
-  $scope.ConfirmTestInput = function(error) {
-    if (error == 422) {
-      showConfirm('ระวัง!!!', 'มีข้อมูลของ ' + $scope.result
-          .tester_tag +
-          ' อยู่แล้ว <br/>ท่านต้องการบันทึกแทนที่หรือไม่?')
-        .then(function(data) {
-          // Results.updateToServer($scope.result);
-          $scope.save();
-        }, function(error) {
-          $scope.clear();
-        })
-    }
-  }
+	$scope.ConfirmTestInput = function(error) {
+		if (error == 422) {
+			showConfirm('ระวัง!!!', 'มีข้อมูลของ ' + $scope.result
+					.tester_tag +
+					' อยู่แล้ว <br/>ท่านต้องการบันทึกแทนที่หรือไม่?')
+				.then(function(data) {
+					Results.getResult({
+							test_id: test_id,
+							course_id: course_id,
+							tester_tag: $scope.result.tester_tag
+						})
+						.then(function(data) {
+							Results.updateToServer({
+									value: $scope.result.value,
+									tester_tag: $scope.result.tester_tag,
+									id: data.id
+								})
+								.then($scope.AlertTestInput, $scope.ConfirmTestInput);
+						})
+
+				}, function(error) {
+					$scope.clear();
+				})
+		} else if (error == 406) {
+			showAlert('ผิดพลาด', 'ผู้เข้าทดสอบหมายเลข ' + $scope.result.tester_tag +
+				' ยัง<b>ไม่ลงทะเบียน</b>')
+		}
+	}
 
 
-  $scope.clear = function() {
-    $scope.result.value = '';
-    $scope.result.tester_tag = '';
-  }
+	$scope.clear = function() {
+		$scope.result.value = '';
+		$scope.result.tester_tag = '';
+	}
 
 
 
-  $scope.save = function() {
-    //validation
+	$scope.save = function() {
+		//validation
 
-    if (!Helper.isNumber($scope.result.value)) {
-      alert($scope.result.value + ' is not real number');
-    } else if (!Helper.isInt($scope.result.tester_tag)) {
-      alert('Tester tag is not integer');
-    } else {
-      Results.saveToServer({
-          'value': $scope.result.value,
-          'test_id': test_id,
-          'tester_tag': $scope.result.tester_tag,
-          'course_id': course_id
-        })
-        .then($scope.AlertTestInput, $scope.ConfirmTestInput);
-    }
-  }
+		if (!Helper.isNumber($scope.result.value)) {
+			alert($scope.result.value + ' is not real number');
+		} else if (!Helper.isInt($scope.result.tester_tag)) {
+			alert('Tester tag is not integer');
+		} else {
+			Results.saveToServer({
+					'value': $scope.result.value,
+					'test_id': test_id,
+					'tester_tag': $scope.result.tester_tag,
+					'course_id': course_id
+				})
+				.then($scope.AlertTestInput, $scope.ConfirmTestInput);
+		}
+	}
 
 })
 
 .controller('SettingCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+	$scope.settings = {
+		enableFriends: true
+	};
 });
--- js/services.js
+++ js/services.js
@@ -72,40 +72,40 @@ angular.module('starter.services', [])
 	// Might use a resource here that returns a JSON array
 
 	// Some fake testing data
-	var results = [{
-		tester_tag: 0,
-		course_name: 'IWING Course',
-		test_name: 'ชั่งน้ำหนัก-BMI',
-		value: '67.5',
-		unit: 'กิโลกรัม'
-	}, {
-		tester_tag: 1,
-		course_name: 'IWING Course',
-		test_name: 'ชั่งน้ำหนัก-BMI',
-		value: '46.95',
-		unit: 'กิโลกรัม'
-	}, {
-		tester_tag: 2,
-		course_name: 'IWING Course',
-		test_name: 'วัดส่วนสูง-BMI',
-		value: '159',
-		unit: 'เซนติเมตร'
-	}, {
-		tester_tag: 3,
-		course_name: 'IWING Course',
-		test_name: 'จำนวนเที่ยว-Pacer',
-		value: '44',
-		unit: 'เที่ยว'
-	}, {
-		id: 4,
-		tester_tag: 4,
-		course_name: 'IWING Course',
-		test_name: 'Curl Up-Curl Up',
-		value: '50',
-		unit: 'รอบ'
-	}];
+	// var results = [{
+	// 	tester_tag: 0,
+	// 	course_name: 'IWING Course',
+	// 	test_name: 'ชั่งน้ำหนัก-BMI',
+	// 	value: '67.5',
+	// 	unit: 'กิโลกรัม'
+	// }, {
+	// 	tester_tag: 1,
+	// 	course_name: 'IWING Course',
+	// 	test_name: 'ชั่งน้ำหนัก-BMI',
+	// 	value: '46.95',
+	// 	unit: 'กิโลกรัม'
+	// }, {
+	// 	tester_tag: 2,
+	// 	course_name: 'IWING Course',
+	// 	test_name: 'วัดส่วนสูง-BMI',
+	// 	value: '159',
+	// 	unit: 'เซนติเมตร'
+	// }, {
+	// 	tester_tag: 3,
+	// 	course_name: 'IWING Course',
+	// 	test_name: 'จำนวนเที่ยว-Pacer',
+	// 	value: '44',
+	// 	unit: 'เที่ยว'
+	// }, {
+	// 	id: 4,
+	// 	tester_tag: 4,
+	// 	course_name: 'IWING Course',
+	// 	test_name: 'Curl Up-Curl Up',
+	// 	value: '50',
+	// 	unit: 'รอบ'
+	// }];
 	var url = 'http://' + Config.HOST + ':8008/result-rest';
-	// var results = window.localStorage.getItem('results');
+	var results = window.localStorage.getItem('results');
 	return {
 		all: function() {
 			return results;
@@ -120,6 +120,35 @@ angular.module('starter.services', [])
 				}
 			}
 			return null;
+		},
+		getResult: function(result) {
+			var deferred = $q.defer();
+			$http({
+					url: url + '/get-result',
+					method: 'POST',
+					data: {
+						'test_id': result.test_id,
+						'tester_tag': result.tester_tag,
+						'course_id': result.course_id
+					},
+					headers: {
+						'Content-Type': 'application/x-www-form-urlencoded'
+					},
+				})
+				.success(function(data, status, headers, config) {
+					deferred.resolve(data);
+				})
+				.error(function(data, status, headers, config) {
+					deferred.reject(status)
+				})
+
+			return deferred.promise;
+		},
+		saveToLocal: function(result) {
+			results.push(result);
+			window.localStorage.setItem('results', results);
+
+			return results;
 		},
 		saveToServer: function(result) {
 			var deferred = $q.defer();
@@ -147,27 +176,39 @@ angular.module('starter.services', [])
 		},
 		updateToServer: function(result) {
 			var deferred = $q.defer();
-			$http({
-					url: url + '/' + result.id,
-					method: 'PUT',
-					data: {
-						'value': result.value,
-						// 'test_id': result.test_id,
-						'tester_tag': result.tester_tag,
-						// 'course_id': result.course_id
-					},
-					headers: {
-						'Content-Type': 'application/x-www-form-urlencoded'
-					},
+			$http.put(url + '/' + result.id, {
+					// url: url + '/' + result.id,
+					// method: 'PATCH',
+					// data: {
+					'value': result.value,
+					// 'test_id': result.test_id,
+					'tester_tag': result.tester_tag,
+					// 'course_id': result.course_id
+					// },
+					// headers: {
+					//   'Content-Type': 'application/x-www-form-urlencoded'
+					// },
 				})
 				.success(function(data, status, headers, config) {
 					deferred.resolve(data);
 				})
 				.error(function(data, status, headers, config) {
-					alert('Can\'t connect to server')
+					// alert('Can\'t connect to server')
+					deferred.reject(status);
 				});
+
+
 
 			return deferred.promise;
 		}
 	};
 })
+
+// .factory('ResultRest', function($resource) {
+//   var url = 'http://' + Config.HOST + ':8008/result-rest/:id';
+//   return $resource(url, null, {
+//     update: {
+//       method: 'PUT'
+//     }
+//   })
+// })
